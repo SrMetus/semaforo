cf4ba11253f5657aa503856a1020955f403b7914
SrMetus <s.gaggero87@gmail.com>
2023-07-26 22:14:36 -0400

    project finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+/*function App() {
+  const [redLight, setLightRed] = useState(false);
+  const [yellowLight, setLightYellow] = useState(false);
+  const [greenLight, setLightGreen] = useState(false);
+
+  const red = () => {
+    setLightRed(!redLight);
+    setLightYellow(false);
+    setLightGreen(false);
+  };
+
+  const yellow = () => {
+    setLightYellow(!yellowLight);
+    setLightRed(false);
+    setLightGreen(false);
+  };
+
+  const green = () => {
+    setLightGreen(!greenLight);
+    setLightRed(false);
+    setLightYellow(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="pole"></div>
+      <div className="shape">
+        <div onClick={red} className={`light red ${redLight ? 'on' : ''}`}></div>
+        <div onClick={yellow} className={`light yellow ${yellowLight ? 'on' : ''}`}></div>
+        <div onClick={green} className={`light green ${greenLight ? 'on' : ''}`}></div>
+      </div>
     </div>
   );
-}
+};
+
+export default App;*/
+
+const  App = () => {
+  const [light, setLight] = useState(false);
+
+  return (
+    <div className="container">
+      <div className="pole"></div>
+      <div className="shape">
+        <div onClick={() => setLight("red")} 
+          className={light === "red" ? "light red on" : "light red"}>
+        </div>
+        <div onClick={() => setLight("yellow")}
+          className={light === "yellow" ? "light yellow on" : "light yellow"}> 
+        </div>
+        <div onClick={() => setLight("green")}
+          className={light === "green" ? "light green on" : "light green"}>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
