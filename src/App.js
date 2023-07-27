13f25dac4d4a273b33481f7c8e7351a8e7fb3e23
SrMetus <s.gaggero87@gmail.com>
2023-07-27 19:24:47 -0400

    Finalizado

--- src/App.js
+++ src/App.js
@@ -1,43 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 
-/*function App() {
-  const [redLight, setLightRed] = useState(false);
-  const [yellowLight, setLightYellow] = useState(false);
-  const [greenLight, setLightGreen] = useState(false);
-
-  const red = () => {
-    setLightRed(!redLight);
-    setLightYellow(false);
-    setLightGreen(false);
-  };
-
-  const yellow = () => {
-    setLightYellow(!yellowLight);
-    setLightRed(false);
-    setLightGreen(false);
-  };
-
-  const green = () => {
-    setLightGreen(!greenLight);
-    setLightRed(false);
-    setLightYellow(false);
-  };
-
-  return (
-    <div className="container">
-      <div className="pole"></div>
-      <div className="shape">
-        <div onClick={red} className={`light red ${redLight ? 'on' : ''}`}></div>
-        <div onClick={yellow} className={`light yellow ${yellowLight ? 'on' : ''}`}></div>
-        <div onClick={green} className={`light green ${greenLight ? 'on' : ''}`}></div>
-      </div>
-    </div>
-  );
-};
-
-export default App;*/
-
 const  App = () => {
   const [light, setLight] = useState(false);
 
